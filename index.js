e9d8f5358e349500e7f7a0d0677ca87d2a07e99b
ThomasScheepers <thomas@scheepers.com>
2023-02-06 15:35:22 +0100

    nu met fallback

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import express from "express"
 // Maak een nieuwe express app aan
 const app = express()
 
+// const res = await fetch('https://whois.fdnd.nl/api/v1/members?first=1');
+// const data = await res.json();
+// console.log(data);
+
 // Stel ejs in als template engine en geef de 'views' map door
 app.set('view engine', 'ejs')
 app.set('views', './views')
